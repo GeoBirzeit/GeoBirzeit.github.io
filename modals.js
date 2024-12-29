a263250720bfec4ed7cbc501eeb290deffd69067
Nada185 <nadaksalamah8@gmail.com>
2024-12-29 21:26:46 +0200

    Update modals.js

--- modals.js
+++ modals.js
@@ -110,35 +110,36 @@ function addRouteToMap(map, routeGeoJSON) {
 // Sample data arrays for categories
 const buildingsData = [
     { 
-        name: "Azeez Shaheen Arts Building", 
-        nodeId: ["NODE_014", "NODE_472", "NODE_473"] 
+        name_EN: "Azeez Shaheen Arts Building", 
+        name_AR: "عزيز شاهين مبنى الاداب", 
+        nodeId: [NODE_014, NODE_472, NODE_473] 
     },
     { 
-        name: "Mohamed Bamieh and Waleed Kayyali Building / Physical Education Building", 
-        nodeId: ["NODE_315"] 
+        name_EN: "Mohamed Bamieh and Waleed Kayyali Building / Physical Education Building", 
+        name_AR: "مبنى محمد بامية ووليد كيالي / مبنى التربية البدنية", 
+        nodeId: [NODE_315] 
     },
     { 
         name_EN: "SHUKY AZEEZ SHAHEEN BUILDING / UNIVERSITY HALLS BUILDING", 
         name_AR: "مبنى شوكي عزيز شاهين / مبنى قاعات الجامعة", 
-        nodeId: ["NODE_316"] 
+        nodeId: [NODE_316] 
     },
     { 
         name_EN: "KINGDOM OF BAHRAIN BUILDING FOR WOMEN STUDIES", 
         name_AR: "مملكة البحرين مبنى لدراسات المرأة", 
-        nodeId: ["NODE_297", "NODE_296"] 
+        nodeId: [NODE_297, NODE_296] 
     },
     { 
         name_EN: "SAID KHOURY BUILDING FOR DEVELOPMENT STUDIES", 
         name_AR: "سعيد خوري مبنى لدراسات التنمية", 
-        nodeId: ["NODE_295"] 
+        nodeId: [NODE_295] 
     },
     { 
         name_EN: "Mohammed Al-Masrouji Media Building", 
         name_AR: "مبنى محمد المسروجي الإعلامي", 
-        nodeId: ["NODE_298", "NODE_299"] 
+        nodeId: [NODE_298, NODE_299] 
     }
 ];
-
 
 const doctorsData = [
     { name: "Dr. Smith - Computer Science", nodeId: [NODE_010] },
